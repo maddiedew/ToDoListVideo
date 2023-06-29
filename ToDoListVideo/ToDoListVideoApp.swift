7e1622d3df22a351baffe441f24af11bb91261c6
Maddie Dew <maddiedew12@gmail.com>
2023-06-29 08:35:14 -0600

    Initial Commit

---
+++ ToDoListVideo/ToDoListVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListVideoApp.swift
+//  ToDoListVideo
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
